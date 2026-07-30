3a20e0ae9ce1500cbb165cc8b18f9227211cdca4
iatealemon <84639864+iatealemon@users.noreply.github.com>
2026-07-31 02:49:41 +0300

    Renamed and fixed local dev

---
+++ local-dev/groups/example/level-objects.d.ts
@@ -0,0 +1,5 @@
+// add objects that exist in the map to get type hints and autocomplete for them when you write code
+
+declare let george: pb2Ragdoll;
+declare let movable: pb2Shape;
+declare let region: pb2Region;
