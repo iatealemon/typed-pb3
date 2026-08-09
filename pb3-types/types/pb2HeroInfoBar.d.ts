f10bd3e674be76881014c1c9483a532ef378af86
iatealemon <84639864+iatealemon@users.noreply.github.com>
2026-08-10 01:01:52 +0300

    pb2HeroInfoBar types

--- pb3-types/types/pb2HeroInfoBar.d.ts
+++ pb3-types/types/pb2HeroInfoBar.d.ts
@@ -6,18 +6,44 @@ declare global {
     interface pb2HeroInfoBar extends ClassIdentityProps<"pb2HeroInfoBar"> {
         /** Type is unknown. One known value: 30 */
         readonly classid: TODO;
-        title: TODO;
-        value: TODO;
-        value_max: TODO;
-        value_handicap: TODO;
-        color: TODO;
-        color_border: TODO;
-        color_delta: TODO;
-        color_handicap: TODO;
-        color_title: TODO;
+        title: string;
+        value: number;
+        value_max: number;
+        value_handicap: number;
+        color: pb2HighRangeColor;
+        color_border: pb2HighRangeColor;
+        color_delta: pb2HighRangeColor | null;
+        color_handicap: pb2HighRangeColor | null;
+        color_title: pb2HighRangeColor;
     }
     var pb2HeroInfoBar: {
-        new (params: TODO): pb2HeroInfoBar;
+        /**
+         * @param params Object containing parameters
+         * @param params.type (default=pb2HeroInfoBar.TYPE_SERVER_VAR)
+         * @param params.order (default=1)
+         * @param params.title (default="")
+         * @param params.value (default=0)
+         * @param params.value_max (default=100)
+         * @param params.value_handicap (default=0)
+         * @param params.color (default=new pb2HighRangeColor(0xd81a1a))
+         * @param params.color_border (default=new pb2HighRangeColor(0xff4747))
+         * @param params.color_delta (default=null)
+         * @param params.color_handicap (default=null)
+         * @param params.color_title (default=new pb2HighRangeColor(0xff4747))
+         */
+        new (params: {
+            type?: TODO,
+            order?: number,
+            title?: string,
+            value?: number,
+            value_max?: number,
+            value_handicap?: number,
+            color?: pb2HighRangeColor,
+            color_border?: pb2HighRangeColor,
+            color_delta?: pb2HighRangeColor,
+            color_handicap?: pb2HighRangeColor,
+            color_title?: pb2HighRangeColor,
+        }): pb2HeroInfoBar;
         /** Type is unknown. One known value: 4 */
         TYPE_SERVER_VAR: TODO;
         /** Type is unknown. One known value: 0 */
